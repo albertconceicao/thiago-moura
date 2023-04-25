c1bab81050f2435fcb813780c8e76d13fb24f91a
Albert Conceição <developer.albert@outlook.com>
2023-04-25 17:17:21 -0300

    fixing width from about and contact image pages

--- src/components/AlternativeHero/styles.ts
+++ src/components/AlternativeHero/styles.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+img {
+    width: 100%;
+    height: 100%;
+}
 @media (max-width: 768px) {
 img {
     display: none;
